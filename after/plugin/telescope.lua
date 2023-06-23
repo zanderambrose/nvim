e95b97d29467fc810812fd84077256f08c371da6
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-06-23 07:54:00 -0400

    Preview shows on small screen width.

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -5,22 +5,20 @@ require('telescope').setup {
                 ["<CR>"] = require('telescope.actions').select_tab,
             },
         },
+        layout_config = {
+            horizontal = {
+                preview_cutoff = 0,
+            },
+        },
     },
 }
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>?', builtin.search_history, {})
+vim.keymap.set('n', '<leader>?', builtin.oldfiles, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>f', builtin.current_buffer_fuzzy_find, {})
 vim.keymap.set('n', '<leader>pa', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
--- Testing themes
-vim.keymap.set('n', '<leader>w', function()
-    builtin.live_grep(require('telescope.themes').get_dropdown {
-        winblend = 10,
-        previewer = false,
-    })
-end, { desc = '[/] Fuzzily search in current buffer' })
