9ee0385214182fc131a318511d749472c125aba5
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-06-23 07:31:51 -0400

    New telescope keybind.

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -11,6 +11,8 @@ require('telescope').setup {
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
+vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>f', builtin.current_buffer_fuzzy_find, {})
+vim.keymap.set('n', '<leader>pa', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
