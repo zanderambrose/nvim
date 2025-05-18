69ea09f94e58bf79ec2046890f02066bbf3cd21a
Zander Ambrose <alexander.d.ambrose@gmail.com>
2025-05-18 18:58:50 -0400

    Add treesitter langs.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
         local configs = require("nvim-treesitter.configs")
 
         configs.setup({
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "go", "gotmpl", "javascript", "typescript", "html", "ruby", "python", "sql", "terraform", "c_sharp", "cpp" },
+            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "go", "gomod", "gowork", "gosum", "gotmpl", "javascript", "typescript", "html", "ruby", "python", "sql", "terraform", "hcl", "c_sharp", "cpp", "dockerfile", "json5", "markdownlint-cli2", "markdown-toc" },
             sync_install = false,
             highlight = { enable = true },
             indent = { enable = true },
