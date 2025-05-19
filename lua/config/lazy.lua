d841a967b17a51a4dfb3c0241558683480559538
Zander Ambrose <alexander.d.ambrose@gmail.com>
2025-05-19 08:42:49 -0400

    Add clipboard opt.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,7 @@ vim.opt.rtp:prepend(lazypath)
 -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+vim.opt.clipboard = "unnamedplus"
 vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
