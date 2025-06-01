63f774930a8cf2a86c154790a65166bc54a4d2aa
Zander Ambrose <alexander.d.ambrose@gmail.com>
2025-05-31 22:08:23 -0400

    Begin avante journey.

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,7 +1,10 @@
 return {
     'saghen/blink.cmp',
     -- optional: provides snippets for the snippet source
-    dependencies = { 'rafamadriz/friendly-snippets' },
+    dependencies = {
+        'rafamadriz/friendly-snippets',
+        'Kaiser-Yang/blink-cmp-avante',
+    },
 
     -- use a release tag to download pre-built binaries
     version = '1.*',
@@ -44,7 +47,16 @@ return {
         -- Default list of enabled providers defined so that you can extend it
         -- elsewhere in your config, without redefining it, due to `opts_extend`
         sources = {
-            default = { 'lsp', 'path', 'snippets', 'buffer' },
+            default = { 'avante', 'lsp', 'path', 'snippets', 'buffer' },
+            providers = {
+                avante = {
+                    module = 'blink-cmp-avante',
+                    name = 'Avante',
+                    opts = {
+                        -- options for blink-cmp-avante
+                    }
+                }
+            },
         },
 
         -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
