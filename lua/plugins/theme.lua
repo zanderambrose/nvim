cb90566c4ff39224dd43c5a0f87f0cf4c824c191
Zander Ambrose <alexander.d.ambrose@gmail.com>
2025-05-31 21:11:10 -0400

    Get rid of nightfox.

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -7,31 +7,3 @@ return {
         vim.cmd("colorscheme tokyonight")
     end
 }
--- return {
---     "EdenEast/nightfox.nvim",
---     priority = 1000,
---     config = function()
---         require("nightfox").setup({
---             options = {
---                 transparent = false,
---                 dim_inactive = true,
---             },
---             palettes = {
---                 carbonfox = {
---                     bg0 = "#0a0a0a",
---                     bg1 = "#0e0e0e",
---                     bg2 = "#1a1a1a",
---                 },
---             },
---             groups = {
---                 carbonfox = {
---                     ["@variable"] = { fg = "#e0af68" },
---                     Visual = { bg = "#444444" },
---                     Normal = { bg = "#0a0a0a" },
---                 },
---             },
---         })
---
---         vim.cmd("colorscheme carbonfox")
---     end,
--- }
