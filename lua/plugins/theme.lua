2b00d92e7f40b34f63efeccc71850725db64f70c
Zander Ambrose <alexander.d.ambrose@gmail.com>
2025-06-20 09:24:33 -0400

    Update transparent theme and avante.

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -2,8 +2,14 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    opts = {},
-    config = function ()
+    config = function()
+        require("tokyonight").setup({
+            transparent = true,
+            styles = {
+                sidebars = "transparent",
+                floats = "transparent",
+            },
+        })
         vim.cmd("colorscheme tokyonight")
     end
 }
